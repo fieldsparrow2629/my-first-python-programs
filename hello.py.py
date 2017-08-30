123f67f749fd4ea0fbe5339e4868f3504a7d94ca
fieldsparrow2629 <31315275+fieldsparrow2629@users.noreply.github.com>
2017-08-30 14:12:55 -0400

    Add files via upload

---
+++ hello.py.py
@@ -0,0 +1,4 @@
+print('hello')
+print('what is your name')
+name = input()
+print('it is good to meet you, ' + name + '.')
---
+++ mad-lib.py
@@ -0,0 +1,7 @@
+print('what is your favorite food?')
+nounf = input()
+print('what is your favorite number?')
+num1 = int(input())
+print('what is your least favorite number?')
+num2 = int(input())
+print(num1 + num2)
